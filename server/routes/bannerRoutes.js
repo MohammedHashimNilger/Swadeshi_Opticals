b45b523438a16df2ee73a18d9d36dab3ea386690
Mohammed Hashim Nilger <hashimrangrezz786@gmail.com>
2026-07-14 01:17:55 +0530

    Update bannerRoutes.js

--- server/routes/bannerRoutes.js
+++ server/routes/bannerRoutes.js
@@ -17,7 +17,4 @@ router.post("/admin", adminProtect, uploadBannerImage, createBanner);
 router.put("/admin/:id", adminProtect, uploadBannerImage, updateBanner);
 router.delete("/admin/:id", adminProtect, deleteBanner);
 
-// Prevent static serving for unmatched routes
-router.get("/*", (req, res) => res.status(404).json({ message: "Route not found" }));
-
 export default router;
